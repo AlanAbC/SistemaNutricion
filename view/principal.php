e6ae53a1b4edfc493257d9ac4b6104de82d3cab6
AlanAbC <alanjiraya77@gmail.com>
2017-05-02 17:29:05 -0500

    Se modificaron estilos

---
+++ view/principal.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Principal</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/estiloPrincipal.css">
+</head>
+<body>
+<div class="head">
+    <img class="logo"></img>
+    <p class="iniciaS">	Iniciar Sesion</p>
+</div>
+<div class="swipper">
+    <a class="abajo" href="#center"></a>
+</div>
+<div class="center" id="center">
+    <h1>¿Ques es Nutrition App?</h1>
+    <p class="infoApp">Nosotros somos ljlkjlkjlskjlksajlaskjslkjslkjaslkjaslkjlksanwaoiduanoisu skaljdsaljdaslk jdsadkljaskdaskdjsal dskajdkl sdjasdl</p>
+    <div class="captura"></div>
+    <p class="registro">Registrarse</p>
+</div>
+<div class="bottom"></div>
+</body>
+</html>
